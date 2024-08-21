8cf30e539aa035d91d2a9f8cbafad2ef11acf5f9
JVivtor <joaovictorsilvaalmeida3@gmail.com>
2024-08-21 17:27:50 -0400

    21/08 melhorias

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -7,9 +7,9 @@ export default function Home(){
     return(
         <>
         <Header/>
-        <p>MINHA LOJA</p>
+        <p>Nike</p>
 
-        <carousel
+        <Carousel
          infiniteLoop
          useKeyboardArrows
          autoPlay
@@ -17,10 +17,12 @@ export default function Home(){
          showStatus={false}
          showThumbs={false}
          dynamicHeight
+        
+         
         >
 
                  <div>
-                    <img src="https://www.nike.com.br/womens-dunk-low-027735.html?cor=MT#pid1" alt="Slide 1" />
+                    <img src="https://imgnike-a.akamaihd.net/768x768/027735MTA2.jpg" alt="Slide 1" />
                 </div> 
                 <div>
                     <img src="https://imgnike-a.akamaihd.net/768x768/027735MTA2.jpg" alt="Slide 2" />
@@ -28,10 +30,20 @@ export default function Home(){
                 <div>
                     <img src="https://imgnike-a.akamaihd.net/768x768/027735MTA1.jpg" alt="Slide 3" />
                 </div>
+                <div>
+                    <img src="https://imgnike-a.akamaihd.net/768x768/029169P1A9.jpg" alt="Slide 4" />
+                </div>
+                <div>
+                    <img src="https://imgnike-a.akamaihd.net/768x768/029169P1A4.jpg" alt="Slide 5" />
+                </div>
+                <div>
+                    <img src="https://imgnike-a.akamaihd.net/768x768/027735MTA2.jpg" alt="Slide 6" />
+                </div>
 
-        </carousel>
+
+        </Carousel>
         </>
-         
 
+        
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Home'
+import Home from '../src/Home'
 import Oferta from './Oferta'
-import Header from './components/Header'
+import Produto from '../src/Produto'
+
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-
+        <Routes>
 <Route path="/" element={<Home />}/>
 <Route path="/Oferta" element={<Oferta />}/>
-<Route path="/components" element={<Header />}/>
-
-    </Routes>
-    
+<Route path="/produtos" element={<Produto/>}/>
+        </Routes>
     </BrowserRouter>
   </React.StrictMode>,
 )

2cda3447744304a9ce2de49d331f59e624af1e9b
JVictor <159329426+JVivtor@users.noreply.github.com>
2024-08-21 09:36:48 -0300

    21/08
    adicionei o carrosel

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,9 +1,35 @@
 import Header from "../components/Header";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+
+
 export default function Home(){
     return(
         <>
         <Header/>
-        <p>pindamonhanganba👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍👍</p>
+        <p>MINHA LOJA</p>
+
+        <carousel
+         infiniteLoop
+         useKeyboardArrows
+         autoPlay
+         showArrows={true}
+         showStatus={false}
+         showThumbs={false}
+         dynamicHeight
+        >
+
+                 <div>
+                    <img src="https://www.nike.com.br/womens-dunk-low-027735.html?cor=MT#pid1" alt="Slide 1" />
+                </div> 
+                <div>
+                    <img src="https://imgnike-a.akamaihd.net/768x768/027735MTA2.jpg" alt="Slide 2" />
+                </div>
+                <div>
+                    <img src="https://imgnike-a.akamaihd.net/768x768/027735MTA1.jpg" alt="Slide 3" />
+                </div>
+
+        </carousel>
         </>
          
 
